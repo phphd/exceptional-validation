8c073114b228f13fb71e3443ef3d901ab5c49e01
Yevhen Sidelnyk <zsidelnik@gmail.com>
2026-09-09 17:28:40 +0300

    wip

--- src/ExceptionalMatcher/Integration/Validator/Middleware/Messenger/Tests/ExceptionalValidationMiddlewareServiceTest.php
+++ src/ExceptionalMatcher/Integration/Validator/Middleware/Messenger/Tests/ExceptionalValidationMiddlewareServiceTest.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace PhPhD\ExceptionalMatcher\Integration\Validator\Middleware\Messenger\Tests;
 
 use PhPhD\ExceptionalMatcher\Bundle\Tests\BundleTestCase;
+use PhPhD\ExceptionalMatcher\Bundle\Tests\PublicServiceCompilerPass;
 use PhPhD\ExceptionalMatcher\Integration\Validator\Middleware\Messenger\ExceptionalValidationMiddleware;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 
 /**
  * @coversNothing
@@ -19,5 +21,11 @@ final class ExceptionalValidationMiddlewareServiceTest extends BundleTestCase
         $middleware = self::getContainer()->get('phd_exceptional_validation');
 
         self::assertInstanceOf(ExceptionalValidationMiddleware::class, $middleware);
+    }
+
+    /** @return list<CompilerPassInterface> */
+    protected static function compilerPasses(): array
+    {
+        return [new PublicServiceCompilerPass('phd_exceptional_validation')];
     }
 }
