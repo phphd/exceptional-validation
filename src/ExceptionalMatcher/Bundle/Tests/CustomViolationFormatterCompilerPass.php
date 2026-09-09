a274f1b4bf11954d5cae85f44a31e31b4dbcddcd
Yevhen Sidelnyk <zsidelnik@gmail.com>
2026-09-09 17:28:14 +0300

    wip

--- src/ExceptionalMatcher/Bundle/Tests/CustomViolationFormatterCompilerPass.php
+++ src/ExceptionalMatcher/Bundle/Tests/CustomViolationFormatterCompilerPass.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace PhPhD\ExceptionalMatcher\Bundle\Tests;
 
 use PhPhD\ExceptionalMatcher\Integration\Validator\Formatter\ExceptionViolationFormatter;
-use PhPhD\ExceptionalMatcher\Mapping\Object\Plan\Registry\ObjectExceptionMappingPlanRegistry;
 use PhPhD\ExceptionalMatcher\Mapping\Object\Property\Catch_\Exception\Formatter\Delegating\Tests\Stub\CustomExceptionViolationFormatter;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\Compiler\ResolveInstanceofConditionalsPass;
@@ -14,26 +13,12 @@ use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 
 /** @internal */
-final class TestServicesCompilerPass implements CompilerPassInterface
+final class CustomViolationFormatterCompilerPass implements CompilerPassInterface
 {
     /** Must outrank {@see ResolveInstanceofConditionalsPass} (100) for interface autoconfiguration to work */
     public const PRIORITY = 105;
 
     public function process(ContainerBuilder $container): void
-    {
-        $this->registerCustomViolationFormatter($container);
-        $this->exposePlanRegistry($container);
-    }
-
-    /** The registry is what most of the mapping is asserted through, yet nothing public leads to it. */
-    private function exposePlanRegistry(ContainerBuilder $container): void
-    {
-        $container->getDefinition(ObjectExceptionMappingPlanRegistry::class)
-            ->setPublic(true)
-        ;
-    }
-
-    private function registerCustomViolationFormatter(ContainerBuilder $container): void
     {
         $container->setDefinition(
             CustomExceptionViolationFormatter::class,
